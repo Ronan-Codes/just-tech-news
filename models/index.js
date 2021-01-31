a74608314dbd75e562df4344eb50f469cbe81c77
Ronan <galv.ronan@gmail.com>
2021-01-31 17:49:41 -0500

    feature/post finished

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User.js');
 const Post = require('./Post.js');
+const Vote = require('./Vote');
 
 // create associations
 User.hasMany(Post, {
@@ -10,6 +11,36 @@ Post.belongsTo(User, {
     foreignKey: 'user_id'
 })
 
+// functions for const Vote
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+})
+
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+})
+
+// const Vote continued, but for Vote connections
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
 
 
-module.exports = { User, Post };
+module.exports = { User, Post, Vote };
